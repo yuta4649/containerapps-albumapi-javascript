2f2b70913310b1e5c17d1514fb30a4bf2941b72d
yuta4649 <72071101+yuta4649@users.noreply.github.com>
2022-11-11 15:07:38 +0900

    Update AlbumController.js

--- controllers/AlbumController.js
+++ controllers/AlbumController.js
@@ -1,6 +1,7 @@
 const Albums = require("../models/Album");
-// test
+
 exports.index = async (req, res) => {
+  const test = "ci/cdテスト用に変数定義
   const albums = await Albums.getAlbums();
   console.log("Retrieved Albums");
   res.json(albums);
